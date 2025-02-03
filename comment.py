66648b78441a961a56db9f7aedbd92eb02a2dd75
Kshatriya0 <iknow433@naver.com>
2025-02-03 21:38:05 +0900

    auto Comment

--- comment.py
+++ comment.py
@@ -6,13 +6,21 @@ import pyautogui
 import time
 
 def clickComment(): 
-    driver.find_element(By.NAME, 'mainFrame').send_keys(Keys.SPACE)
+    F = driver.find_element(By.NAME, 'mainFrame')
+    F.send_keys(Keys.SPACE)
     time.sleep(2)
     driver.switch_to.frame("mainFrame")
+
     try:
         driver.find_element(By.XPATH, '//*[@id="btn_comment_2"]').click()
+    
     except:
-        driver.find_element(By.CLASS_NAME, 'u_cbox_inbox').click()
+        print("Ex")
+        driver.switch_to.default_content()
+        F.send_keys(Keys.SPACE)
+        driver.switch_to.frame("mainFrame")
+        driver.find_element(By.XPATH, '//*[@id="btn_comment_2"]').click()
+    
     time.sleep(2)
 
 def Login(myId, myPw):
@@ -26,14 +34,14 @@ def Login(myId, myPw):
     idBox = driver.find_element(By.ID, "id")
     idBox.click()
     pyperclip.copy(myId)
-    pyautogui.hotkey("command")
-    pyautogui.hotkey("command", "v")
+    pyautogui.hotkey("ctrl")
+    pyautogui.hotkey("ctrl", "v")
     time.sleep(1)  
     
     pwBox = driver.find_element(By.ID, "pw")
     pwBox.click()
     pyperclip.copy(myPw)
-    pyautogui.hotkey("command", "v")
+    pyautogui.hotkey("ctrl", "v")
     time.sleep(1)
 
     loginBtn = driver.find_element(By.ID, "log.login")
@@ -44,23 +52,60 @@ def Login(myId, myPw):
 
 
 def makeComment(comment):
+    driver.find_element(By.CLASS_NAME, 'u_cbox_inbox').click()
     pyperclip.copy(comment)
-    pyautogui.hotkey("command", "v")
+    pyautogui.hotkey("ctrl", "v")
     time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, '.u_cbox .u_cbox_btn_upload').click()
+    time.sleep(1)
+    
+
+
+def findFirstPost():
+    driver.switch_to.frame("mainFrame")
+    mainPage = driver.find_element(By.XPATH, '//*[@id="blog-menu"]/div/table/tbody/tr/td[1]/ul/li[2]/a')
+    driver.execute_script("arguments[0].click();", mainPage)
     time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="categoryTitle"]').click()
+    time.sleep(2)
+    firstPost = driver.find_element(By.XPATH, '//*[@id="listTopForm"]/table/tbody/tr[1]/td[1]/div')
+    firstPost.click()
+    time.sleep(3)
+    driver.switch_to.default_content()
+
 
 myId = input("Id를 입력하세요")
 myPw = input("Pw를 입력하세요")
+comment = "잘 보고 갑니다~"
+
 
 url = input("블로그 링크를 입력해주세요")
 driver = webdriver.Chrome() 
 driver.get(url)
-time.sleep(3)
-
-comment = "안녕하세요 이웃님, 좋은 게시글 잘 읽고 갑니다! \n 연말 즐겁게 보내시고 새해 복 많이 받으세요~"
+time.sleep(2)
 
 clickComment()
 Login(myId, myPw)
-clickComment()
-makeComment(comment)
+F = driver.find_element(By.NAME, 'mainFrame')
+F.send_keys(Keys.SPACE)
+time.sleep(2)
+driver.switch_to.frame("mainFrame")
+profile = driver.find_elements(By.CLASS_NAME, "u_cbox_nick")
+main_tab = driver.window_handles[0]
+time.sleep(1)
+for i in range(len(profile)):
+    profile[i].click()
+    time.sleep(2)
+    new_tab = driver.window_handles[1]
+    driver.switch_to.window(new_tab)
+    findFirstPost()
+    if True:
+        clickComment()
+        makeComment(comment)
+    time.sleep(1)
+    pyautogui.hotkey("ctrl", "w") #Close tab
+    time.sleep(1)
+    driver.switch_to.window(main_tab)
+    driver.switch_to.frame("mainFrame")
+
+print("Success")
